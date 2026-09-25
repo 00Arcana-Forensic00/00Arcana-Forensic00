886e91ad27e8637ea06c0d4e664204fb0f2938cc
mitchell5584dm-tech <mitchell5584.dm@gmail.com>
2026-09-25 03:22:39 -0400

    Connect live Stripe payment links and inventory tier

--- site/stripe.config.js
+++ site/stripe.config.js
@@ -1,6 +1,10 @@
 window.ARCANA_STRIPE = {
-  professionalUrl: "",
-  enterpriseUrl: "",
+  communityInventoryUrl: "https://buy.stripe.com/dRm8wO0vIesA0Yg1Jn5sA0j",
+
+  professionalUrl: "https://buy.stripe.com/7sY9AS2DQbgo6iAds55sA0i",
+
+  enterpriseUrl: "https://buy.stripe.com/4gMdR8a6i4S0cGY5ZD5sA0h",
+
   servicesUrl: "mailto:sales@arcana-forensics.com",
 
   successUrl: "https://arcana-forensics.com/site/checkout-success.html",
@@ -8,8 +12,8 @@ window.ARCANA_STRIPE = {
 
   packetId: "AF-FT-2026-0921",
 
-  professionalUsd: 875,
-  enterpriseUsd: 8750,
+  professionalUsd: 3500,
+  enterpriseUsd: 35000,
   governmentFromUsd: 150000,
   servicesMinUsd: 250,
   servicesMaxUsd: 500
